89dd8579dbe64b6456ae1a1df658b0b8aa6fda15
Natalia Sekulic <natalia.sekulic1@gmail.com>
2026-08-21 06:02:38 -0500

    Add file for [Module 2] Lab 1: Simple Linear Regression

---
+++ Lab1.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# load dataset into a pandas DataFrame (table)
+url= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+df=pd.read_csv(url)
+print(df.describe())
+
+# create smaller DF with selected relevant features
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+
+# extract input feature, X, and target output variable y from the dataset
+X = cdf.ENGINESIZE.to_numpy()
+y = cdf.CO2EMISSIONS.to_numpy()
+
+# split dataset into 80% training and 20% testing data
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+# create a model object
+regressor = linear_model.LinearRegression()
+
+# train the model on the training data
+# X_train is a 1-D array but sklearn models expect a 2D array as input for the training data, with shape (n_observations, n_features).
+# So we need to reshape it. We can let it infer the number of observations using '-1'.
+regressor.fit(X_train.reshape(-1, 1), y_train)
+print ('Coefficients: ', regressor.coef_[0]) # with simple linear regression there is only one coefficient, here we extract it from the 1 by 1 array.
+print ('Intercept: ',regressor.intercept_)
+
+# plot the regression model over the test data and visually evaluate
+plt.scatter(X_test, y_test, color='blue')
+plt.plot(X_test, regressor.coef_ * X_test + regressor.intercept_, '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+#plt.show()
+
+# use the predict method to make test predictions
+y_pred = regressor.predict(X_test.reshape(-1,1))
+
+# calculate and print the Mean Squared Error (MSE) of the test predictions
+print("MSE: %.2f" % mean_squared_error(y_test, y_pred))
